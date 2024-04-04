9d8dbe5ba3820e04e86e099812ab86ea815afc6c
codewithdivu <mavadiyadivyesh56@gmail.com>
2024-04-04 17:29:28 +0530

    Initial Commit

---
+++ screen_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  screen_2
+//
+//  Created by Divyesh  on 04/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
